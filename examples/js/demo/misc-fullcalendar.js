85beabe475bf0268239cb44d4a2cf47554e7abe8
Edsel Serrano <pretxel@hotmail.com>
2015-06-08 13:28:17 -0500

    change page lugar.html

--- examples/js/demo/misc-fullcalendar.js
+++ examples/js/demo/misc-fullcalendar.js
@@ -60,87 +60,29 @@ $(document).ready(function() {
 		eventLimit: true, // allow "more" link when too many events
 		events: [
 			{
-				title: 'Happy Hour',
+				title: 'Arduino',
 				start: '2015-01-05',
 				end: '2015-01-07',
 				className: 'purple'
 			},
 			{
-				title: 'Birthday Party',
+				title: 'Labview',
 				start: '2015-01-15',
 				end: '2015-01-17',
 				className: 'mint'
 			},
 			{
-				title: 'All Day Event',
-				start: '2015-01-15',
-				className: 'warning'
-			},
-			{
-				title: 'Meeting',
+				title: 'Mecánica',
 				start: '2015-01-20T10:30:00',
 				end: '2015-01-20T12:30:00',
 				className: 'danger'
 			},
 			{
-				title: 'All Day Event',
+				title: 'Labview',
 				start: '2015-02-01',
-				className: 'warning'
-			},
-			{
-				title: 'Long Event',
-				start: '2015-02-07',
-				end: '2015-02-10',
-				className: 'purple'
-			},
-			{
-				id: 999,
-				title: 'Repeating Event',
-				start: '2015-02-09T16:00:00'
-			},
-			{
-				id: 999,
-				title: 'Repeating Event',
-				start: '2015-02-16T16:00:00',
-				className: 'success'
-			},
-			{
-				title: 'Conference',
-				start: '2015-02-11',
-				end: '2015-02-13',
-				className: 'dark'
-			},
-			{
-				title: 'Meeting',
-				start: '2015-02-12T10:30:00',
-				end: '2015-02-12T12:30:00'
-			},
-			{
-				title: 'Lunch',
-				start: '2015-02-12T12:00:00',
-				className: 'pink'
-			},
-			{
-				title: 'Meeting',
-				start: '2015-02-12T14:30:00'
-			},
-			{
-				title: 'Happy Hour',
-				start: '2015-02-12T17:30:00'
-			},
-			{
-				title: 'Dinner',
-				start: '2015-02-12T20:00:00'
-			},
-			{
-				title: 'Birthday Party',
-				start: '2015-02-13T07:00:00'
-			},
-			{
-				title: 'Click for Google',
-				url: 'http://google.com/',
-				start: '2015-02-28'
+				className: 'mint'
 			}
+			
 		]
 	});
 
